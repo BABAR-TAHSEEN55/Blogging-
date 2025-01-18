e5a75ef6611f1b687bcec4e494a680609a3228ec
Babar <babarsmoke24@gmail.com>
2025-01-18 22:55:38 +0530

    Authentication created

---
+++ utils/authentication.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+const secret = process.env.SECRET;
+// console.log(secret);
+
+const CreateToken = (user) => {
+  const payload = {
+    _id: user._id,
+    Email: user.Email,
+    FullName: user.FullName,
+  };
+  const token = jwt.sign(payload, secret);
+  return token;
+};
+
+const ValidateToken = (token) => {
+  const payload = jwt.verify(token, secret, {
+    expiresIn: "1d",
+  });
+  return payload;
+};
+export default { CreateToken, ValidateToken };
