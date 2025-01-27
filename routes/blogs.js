b31b9171441fa02ff1a8acf809e89eb739a4723c
Babar <babarsmoke24@gmail.com>
2025-01-27 22:39:48 +0530

    Route for Blog Implemented

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,10 +1,27 @@
 import { Router } from "express";
 const router = Router();
-
-router.get("/add-new-blog", (req, res) => {
-  res.render("addblogs", {
-    user: req.user,
+import multer from "multer";
+import { upload } from "../middlewares/multer.js";
+import { blog } from "../models/blogs.js";
+router
+  .get("/add-new-blog", (req, res) => {
+    res.render("addblogs", {
+      user: req.user,
+    });
+  })
+  .post("/add-new-blog", upload.single("CoverImage"), async (req, res) => {
+    const { Title, Body } = req.body;
+    const CreateBlog = await blog.create({
+      Title,
+      Body,
+      CoverImage: `/uploads/${req.file.filename}`,
+      CreatedBy: req.user._id,
+    });
+    console.log(CreateBlog);
+    // return res.render("Card", {
+    //   CoverImage: `../public/uploads/${req.file.filename}`,
+    // });
+    // return res.redirect(`/CreateBlog/${CreateBlog._id}`);
+    return res.redirect("/");
   });
-});
-
 export default router;
